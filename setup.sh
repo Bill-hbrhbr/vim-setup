c2cc1929607cfb949811525bafeca8e5ea2dedee
Bingran Hu <bingran.hu@yscope.com>
2024-08-24 02:02:07 -0400

    Add git configs

--- setup.sh
+++ setup.sh
@@ -2,9 +2,14 @@
 
 rm -f ~/.vimrc
 rm -rf ~/.vim
+rm -f ~/.tmux.conf
+rm -f ~/.gitconfig
+rm -f ~/.gitignore
+
 git submodule update --init --recursive
 cp .vimrc ~/
 cp -r .vim ~/
 cp .tmux.conf ~/
 tmux source ~/.tmux.conf
-
+cp .gitconfig ~/
+cp .gitignore ~/
