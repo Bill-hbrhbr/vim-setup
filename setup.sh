fd9978e90c0ba1f4e3df306427d049e30cebbc11
Bingran Hu <bingran.hu@yscope.com>
2024-11-11 23:22:25 -0500

    Add java

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ rm -rf ~/.vim
 rm -f ~/.tmux.conf
 rm -f ~/.gitconfig
 rm -f ~/.gitignore
+rm -rf ~/myjdk
 
 git submodule update --init --recursive
 cp .vimrc ~/
@@ -13,3 +14,14 @@ cp .tmux.conf ~/
 tmux source ~/.tmux.conf
 cp .gitconfig ~/
 cp .gitignore ~/
+
+cd ~ && mkdir myjdk && cd myjdk
+wget jdk-23_linux-x64_bin.tar.gz
+mkdir jdk-23
+tar xzf jdk-23_linux-x64_bin.tar.gz -C jdk-23 --strip-components 1
+
+echo "" >> ~/.bashrc
+echo "export JAVA_HOME=~/jdk/jdk-23" >> ~/.bashrc
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
+
