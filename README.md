46fb943cce3963e1f589e5917aaf23a25829d942
Bingran Hu <bingran.hu@yscope.com>
2024-11-21 07:36:34 +0000

    Update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # vim-setup
 
 ```shell
-pushd ~
+mkdir ~/.local
+pushd ~/.local
 git clone https://github.com/Bill-hbrhbr/vim-setup.git
 cd vim-setup && ./setup.sh
 popd
+
 ```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+    vim \
+    tmux \
+    wget
+
 rm -f ~/.vimrc
 rm -rf ~/.vim
 rm -f ~/.tmux.conf
@@ -15,14 +21,16 @@ tmux source ~/.tmux.conf
 cp .gitconfig ~/
 cp .gitignore ~/
 
-cd ~ && mkdir myjdk && cd myjdk
-wget https://download.oracle.com/java/23/latest/jdk-23_linux-x64_bin.tar.gz
-mkdir jdk-23
-tar xzf jdk-23_linux-x64_bin.tar.gz -C jdk-23 --strip-components 1
-
 # java
+mkdir -p ~/.local
+cd ~/.local
+wget https://download.oracle.com/java/23/latest/jdk-23_linux-x64_bin.tar.gz
+rm -rf jdk-23 && mkdir jdk-23
+tar xzf jdk-23_linux-x64_bin.tar.gz -C jdk-23 --strip-components 1
+rm jdk-23_linux-x64_bin.tar.gz
+
 echo "" >> ~/.bashrc
-echo "export JAVA_HOME=~/myjdk/jdk-23" >> ~/.bashrc
+echo "export JAVA_HOME=~/.local/jdk-23" >> ~/.bashrc
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
 
