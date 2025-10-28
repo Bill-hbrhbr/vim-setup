62ebdf9b5b5f43a88cf5c56d3bdc6d5c1dfe12ab
Bingran Hu <bingranhu98@gmail.com>
2025-10-28 22:34:45 +0800

    Refactor JDK and task installation in setup.sh
    Updated JDK installation to use variable for version and changed task installation version.

--- setup.sh
+++ setup.sh
@@ -22,22 +22,27 @@ cp .gitconfig ~/
 cp .gitignore ~/
 
 # java
+JDK_VERSION=25
+
 mkdir -p ~/.local
 cd ~/.local
-wget https://download.oracle.com/java/23/latest/jdk-23_linux-x64_bin.tar.gz
-rm -rf jdk-23 && mkdir jdk-23
-tar xzf jdk-23_linux-x64_bin.tar.gz -C jdk-23 --strip-components 1
-rm jdk-23_linux-x64_bin.tar.gz
 
-echo "" >> ~/.bashrc
-echo "export JAVA_HOME=~/.local/jdk-23" >> ~/.bashrc
-echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+wget "https://download.oracle.com/java/${JDK_VERSION}/archive/jdk-${JDK_VERSION}_linux-x64_bin.tar.gz"
+rm -rf "jdk-${JDK_VERSION}" && mkdir "jdk-${JDK_VERSION}"
+tar xzf "jdk-${JDK_VERSION}_linux-x64_bin.tar.gz" -C "jdk-${JDK_VERSION}" --strip-components 1
+rm "jdk-${JDK_VERSION}_linux-x64_bin.tar.gz"
+
+{
+  echo ""
+  echo "export JAVA_HOME=\$HOME/.local/jdk-${JDK_VERSION}"
+  echo "export PATH=\$JAVA_HOME/bin:\$PATH"
+} >> ~/.bashrc
 source ~/.bashrc
 
 # task
 mkdir -p ~/.local/bin
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d v3.36.0
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d v3.44.0
 echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
 
